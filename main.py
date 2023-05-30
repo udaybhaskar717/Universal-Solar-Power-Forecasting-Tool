1ada0c26cda8ac53a8c1e2a5e398b03a30ad0bd3
udaybhaskar717 <79568421+udaybhaskar717@users.noreply.github.com>
2023-05-30 16:44:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ Original file is located at
 """
 
 import streamlit as st
-from process import SolarDataProcessor
+from solardataprocessor import SolarDataProcessor
 from model import TransferLearningModel
 
 # Constants
